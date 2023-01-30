6cd54fe118adcf713e5bf0b8d801c39d496dee36
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-01-30 15:56:47 +0300

    navbar js

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
-function navToggle() {
-    var x = document.getElementById("menus");
-    var logo = document.getElementById("logo");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
+const menuBtn = document.querySelector(".bar-toggle");
+	const menu =
+		document.querySelector(".menu");
 
-function myFunction(x) {
-    x.classList.toggle("fa-minus-circle");
+	const menuNav =
+		document.querySelector(".navbar-links");      
+const navItems = document.querySelectorAll(".nav-item");
+const navbarLinks = document.querySelector(".navbar-links");
+
+// Set the initial state of the menu
+let showMenu = true;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
+
+    // Reset the menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
+
+    // Reset the menu state
+    showMenu = false;
+  }
 }
