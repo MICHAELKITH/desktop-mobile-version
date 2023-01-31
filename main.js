e3e32691b840a59ab1e45a6b4b1b1183ec04ab3c
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-02-01 00:17:47 +0300

    changes

--- main.js
+++ main.js
@@ -16,3 +16,37 @@ navItems.forEach((link) => {
     menu.classList.toggle('hidden');
   });
 });
+
+
+const modalBtn =document.querySelectorAll('[data-modal-target]')
+const closeModalBtn =document.querySelectorAll('[data-close-btn]')
+const overlay = document.getElementById('overlay')
+
+overlay.addEventListener('click', ()=>{
+  const modals =document.querySelectorAll('.modal.active')
+  modals.forEach(modal =>{
+    closeModal(modal)
+  })
+})
+modalBtn.forEach(button =>{
+  button.addEventListener('click', ()=>{
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+closeModalBtn.forEach(button =>{
+  button.addEventListener('click', ()=>{
+    const modal = button.closest('.modal')
+   closeModal(modal)
+  })
+})
+function openModal(modal){
+  if(modal==null)return 
+  modal.classList.add("active")
+ overlay.classList.add("active")
+}
+function closeModal(modal){
+  if(modal==null)return 
+  modal.classList.remove("active")
+ overlay.classList.remove("active")
+}
